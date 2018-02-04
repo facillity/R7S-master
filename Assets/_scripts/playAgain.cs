4af3a3956e0957ac1894b7d1b30843598140f029
David Leung <DaveedL@users.noreply.github.com>
2018-02-03 23:25:08 -0800

    Added dynamic text field to Player winner

--- Assets/_scripts/playAgain.cs
+++ Assets/_scripts/playAgain.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class playAgain : MonoBehaviour {
 
+    public Text myText;
 
+    public void Update()
+    {
+        myText.text = "5"; //allVariables.winner.toString()
+    }
     public void changeMenuScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
